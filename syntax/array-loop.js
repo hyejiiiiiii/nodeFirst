472baa7344ccbe672d125e38a1ff1aa2234b4568
hyejiiiiii <hyeji972@gmail.com>
2020-03-09 18:20:20 +0900

    new file

---
+++ syntax/array-loop.js
@@ -0,0 +1,5 @@
+var total=0;
+for(var i=0; i<4; i++){
+  total+=i;
+}
+console.log(`total : ${total}`);
---
+++ syntax/array.js
@@ -0,0 +1,5 @@
+var hyeji=['hyeji','hyeji2'];
+console.log(hyeji);
+hyeji.push('asdfasfdsadf');
+console.log(hyeji);
+console.log(hyeji[2]);
